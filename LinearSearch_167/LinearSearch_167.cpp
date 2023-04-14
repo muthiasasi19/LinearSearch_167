f99c1acf66baba73699567caab6181ffdd00a61c
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-13 22:45:56 -0700

    langkah 2, 3, dan 4

--- LinearSearch_167/LinearSearch_167.cpp
+++ LinearSearch_167/LinearSearch_167.cpp
@@ -40,3 +40,6 @@ void LinearSearch()
 		cout << "\nEnter the element you want to search: "; //Langkah 1
 		int item;
 		cin >> item;
+
+		comparison = 0;
+		for (i = 0; i < n; i++)								// Langkah 2,3 dan 4
