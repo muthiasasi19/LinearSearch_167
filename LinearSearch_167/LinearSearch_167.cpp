fb956cb3b862681bbb36a8e0f36da5e9edd00f08
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-13 22:46:53 -0700

    langkah 5

--- LinearSearch_167/LinearSearch_167.cpp
+++ LinearSearch_167/LinearSearch_167.cpp
@@ -43,3 +43,12 @@ void LinearSearch()
 
 		comparison = 0;
 		for (i = 0; i < n; i++)								// Langkah 2,3 dan 4
+
+		{
+		comparison++;
+			if (arr[i] == item)							// Langkah 5 a found
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
